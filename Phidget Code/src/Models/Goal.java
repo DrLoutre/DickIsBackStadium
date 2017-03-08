0bccca7cae5050fb7ab02c71f948fefa724502bc
Brieuc <brieuc.olivier@student.unamur.be>
2017-03-08 11:41:26 +0100

    Goals fonctionnels

--- Phidget Code/src/Models/Goal.java
+++ Phidget Code/src/Models/Goal.java
@@ -4,6 +4,8 @@ import com.phidgets.InterfaceKitPhidget;
 import com.phidgets.PhidgetException;
 import com.phidgets.event.InputChangeEvent;
 import com.phidgets.event.InputChangeListener;
+import com.phidgets.event.SensorChangeEvent;
+import com.phidgets.event.SensorChangeListener;
 
 /**
  * Created by bri_e on 07-03-17.
@@ -16,6 +18,7 @@ public class Goal {
     private int indexIRSensor;
     private int indexVibrationSensor;
     private int goal;
+    private double lastGoal;
     private InterfaceKitPhidget interfaceKitPhidget;
 
     public Goal(InterfaceKitPhidget ifk, int indexPassage, int indexVibration) throws PhidgetException {
@@ -25,36 +28,45 @@ public class Goal {
         passageHappend = 0;
         vibrationHappend = 0;
         goal = 0;
-        ifk.addInputChangeListener(new InputChangeListener() {
+        lastGoal=0;
+        ifk.addSensorChangeListener(new SensorChangeListener() {
             @Override
-            public void inputChanged(InputChangeEvent inputChangeEvent) {
-                System.out.println("Passage !");
-                try {
-                    if (ifk.getSensorValue(indexIRSensor) < 100) {
-                        passageHappend = System.currentTimeMillis();
-                    }
-                } catch(PhidgetException e) {
-                    System.out.println("Exception à la lecture du phidget de proximité : " + e);
+            public void sensorChanged(SensorChangeEvent sensorChangeEvent) {
+                switch(sensorChangeEvent.getIndex()) {
+                    case 3 :
+                        //System.out.println("Passage !");
+                        try {
+                            if (ifk.getSensorValue(indexIRSensor) < 100) {
+                                passageHappend = System.currentTimeMillis();
+                            }
+                        } catch (PhidgetException e) {
+                            System.out.println("Exception à la lecture du phidget de proximité : " + e);
+                        }
+                        break;
+                    case 4:
+                        //System.out.println("Vibration !");
+                        try {
+                            if (ifk.getSensorValue(indexVibrationSensor) > 700) {
+                                vibrationHappend = System.currentTimeMillis();
+                            }
+                        } catch(PhidgetException e){
+                            System.out.println("Exception à la lecture du phidget de vibration : " + e);
+                        }
+                        if(((vibrationHappend-passageHappend)<10)){
+                            if((int)(System.currentTimeMillis()-lastGoal)>20000){
+                                lastGoal = System.currentTimeMillis();
+                                goal++;
+                                System.out.println("Gooaaaaaaaaaaal !!!!!!  " + getGoal());
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+
                 }
             }
         });
-        ifk.addInputChangeListener(new InputChangeListener() {
-            @Override
-            public void inputChanged(InputChangeEvent inputChangeEvent) {
-                System.out.println("Vibration !");
-                try {
-                    if (ifk.getSensorValue(indexVibration) > 700) {
-                        vibrationHappend = System.currentTimeMillis();
-                    }
-                } catch(PhidgetException e){
-                    System.out.println("Exception à la lecture du phidget de vibration : " + e);
-                }
-                if((vibrationHappend-passageHappend)<10){
-                    goal++;
-                    System.out.println("Gooaaaaaaaaaaal !!!!!!");
-                }
-            }
-        });
+
     }
 
     public int getGoal(){
