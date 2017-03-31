f8dfab8e88dd80a97f570489ecd134fc0f23fa15
Jordan Andreux <jordan.andreux@student.unamur.be>
2017-03-31 09:49:08 +0200

    [Web] Add first bean

---
+++ Maven/Stade/src/main/java/beans/Buvette.java
@@ -0,0 +1,36 @@
+package beans;
+
+public class Buvette {
+
+    private int Id;
+    private float Frequentation;
+    private String Localisation;
+
+    public Buvette() {
+
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public float getFrequentation() {
+        return Frequentation;
+    }
+
+    public void setFrequentation(float frequentation) {
+        Frequentation = frequentation;
+    }
+
+    public String getLocalisation() {
+        return Localisation;
+    }
+
+    public void setLocalisation(String localisation) {
+        Localisation = localisation;
+    }
+}
