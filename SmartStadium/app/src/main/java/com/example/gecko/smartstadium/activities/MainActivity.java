b06dec6680d7e30e0c915429f0d8edc3fe0fc0a4
DESKTOP-6GD5MQQ\Gecko <quentin.jacquemotte@student.unamur.be>
2017-04-04 09:15:32 +0200

    [ANDROID] ecran d'aceuil + QRcode + bug fixe ( de qq'un mais il assume pas :p )

--- SmartStadium/app/src/main/java/com/example/gecko/smartstadium/activities/MainActivity.java
+++ SmartStadium/app/src/main/java/com/example/gecko/smartstadium/activities/MainActivity.java
@@ -1,15 +1,44 @@
 package com.example.gecko.smartstadium.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.gecko.smartstadium.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button NFCButton;
+    private Button QRButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NFCButton = (Button) findViewById(R.id.loginButtonNFC);
+        QRButton = (Button) findViewById(R.id.loginButtonQRcode);
+
+        //todo ajouter la demande de permission
+
+        NFCButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo changer pour aller vers le lecteur NFC
+                Intent intent = new Intent(MainActivity.this, QRCodeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        QRButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QRCodeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ SmartStadium/app/src/main/java/com/example/gecko/smartstadium/activities/QRCodeActivity.java
@@ -0,0 +1,93 @@
+package com.example.gecko.smartstadium.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+import com.example.gecko.smartstadium.R;
+
+
+/**
+ * This activity handle the decoding of the QRcode
+ *
+ * @author Quentin Jacquemotte
+ */
+public class QRCodeActivity extends Activity implements QRCodeReaderView.OnQRCodeReadListener {
+
+    Intent intent;
+    private QRCodeReaderView qrCodeReaderView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode);
+        qrCodeReaderView = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        qrCodeReaderView.setOnQRCodeReadListener(this);
+
+
+        /**
+         * Use this function to enable/disable decoding
+         */
+        qrCodeReaderView.setQRDecodingEnabled(true);
+
+        /**
+         * Use this function to change the autofocus interval (default is 5 secs)
+         */
+        qrCodeReaderView.setAutofocusInterval(2000L);
+
+        /**
+         * Use this function to enable/disable Torch
+         */
+        qrCodeReaderView.setTorchEnabled(true);
+
+        /**
+         *  Use this function to set front camera preview
+         */
+        qrCodeReaderView.setFrontCamera();
+
+        /**
+         * Use this function to set back camera preview
+         */
+        qrCodeReaderView.setBackCamera();
+
+        Snackbar.make(findViewById(android.R.id.content), "Scannez un QR code...", Snackbar.LENGTH_LONG).show();
+
+    }
+
+
+    /**
+     * Called when a QR is decoded
+     *
+     * @param text   the text encoded in QR
+     * @param points points where QR control points are placed in View
+     */
+    public void onQRCodeRead(String text, PointF[] points) {
+
+        Intent returnIntent = new Intent(); //todo déffinir vers ou aller
+        returnIntent.putExtra("result", text);
+        startActivity(returnIntent);
+        finish();
+    }
+
+
+    /**
+     * Start the camera
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        qrCodeReaderView.startCamera();
+    }
+
+    /**
+     * Stop the camera
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        qrCodeReaderView.stopCamera();
+    }
+}
