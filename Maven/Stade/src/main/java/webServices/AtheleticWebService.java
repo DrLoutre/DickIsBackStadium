1ca7f58e6730bb54cbef5ad86842d85b1436c716
Jordan Andreux <jordan.andreux@student.unamur.be>
2017-04-05 12:06:00 +0200

    [Web] Add Get Athletic WebService

--- Maven/Stade/src/main/java/webServices/AtheleticWebService.java
+++ Maven/Stade/src/main/java/webServices/AtheleticWebService.java
@@ -1,18 +1,36 @@
 package webServices;
 
-import beans.Credentials;
 import beans.Athletic;
+import beans.Credentials;
+import dao.impl.AthleticDaoImpl;
+import exceptions.NotFoundException;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Path("atheletics")
 public class AtheleticWebService {
+
+    private AthleticDaoImpl athleticDao;
+
+    @Context
+    public void setContext() {
+        athleticDao = new AthleticDaoImpl();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getAthletic(@PathParam("id") String id) {
+        try {
+            Athletic athletic = athleticDao.getAthletic(id);
+            return Response.ok(athletic).build();
+        } catch (NotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
 
     @POST
     @Path("/login")
