e5a19a7f4efacf47bc8308ad36f0e6b4b85f8310
Brieuc <brieuc.olivier@student.unamur.be>
2017-04-20 09:48:35 +0200

    Starting creation of project structure

--- Phidget Code/src/Models/In/CommunicationListener.java
+++ Phidget Code/src/Models/In/CommunicationListener.java
@@ -20,5 +20,4 @@ public class CommunicationListener {
 
     //Todo : Parse from the received request
 
-
 }
---
+++ PhidgetScalaCode/src/Models/In/Goal.scala
@@ -0,0 +1,10 @@
+package Models.In
+
+/**
+  * Created by bri_e on 20-04-17.
+  * This object represent a goal, we listen to both passage event and vibration event.
+  * With a calculation on the time lapse between both event in a correct order we will determinate if there is a goal or not
+  */
+class Goal {
+
+}
