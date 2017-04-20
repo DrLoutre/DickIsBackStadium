b15e660dca526d46e83d5ccd0db25751a5ea9462
Jordan Andreux <jordan.andreux@student.unamur.be>
2017-04-20 10:29:26 +0200

    Get infos from Arduino

--- StadiumWebSite/src/main/java/webServices/RefreshmentWebService.java
+++ StadiumWebSite/src/main/java/webServices/RefreshmentWebService.java
@@ -7,6 +7,8 @@ import exceptions.NotFoundException;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.*;
+import java.net.*;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Path("refreshments")
@@ -17,8 +19,16 @@ public class RefreshmentWebService {
     @GET
     @Path("/{id}")
     public Response getRefreshment(@PathParam("id") int id) {
+
+        if (id < 1 || id > 2) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        String pourcentage;
         Refreshment refreshment;
         try {
+            pourcentage = getUrl(String.valueOf(id));
+            refreshmentDao.setAttendance(id, Float.valueOf(pourcentage));
             refreshment = refreshmentDao.getRefreshment(id);
         } catch (NotFoundException e) {
             return Response.status(Response.Status.NOT_FOUND).build();
@@ -44,5 +54,37 @@ public class RefreshmentWebService {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
         return Response.ok(refreshment1).build();
+    }
+
+    private String getUrl(String id) throws NotFoundException {
+        HttpURLConnection connection = null;
+        StringBuilder output = new StringBuilder();
+
+        try {
+            //Create connection
+            URL url = new URL("http://192.168.1.4/G" + id);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            //Get Response
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                output.append(inputLine);
+            }
+
+            br.close();
+
+            return output.toString();
+        } catch (Exception e) {
+            throw new NotFoundException();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
     }
 }
