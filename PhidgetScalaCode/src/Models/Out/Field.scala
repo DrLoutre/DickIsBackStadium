776b19bab6765b31b038a051caf9950dfba9dc61
Brieuc <brieuc.olivier@student.unamur.be>
2017-04-21 10:29:29 +0200

    field done

--- PhidgetScalaCode/src/Models/Out/Field.scala
+++ PhidgetScalaCode/src/Models/Out/Field.scala
@@ -1,9 +1,47 @@
 package Models.Out
 
+import com.phidgets.{InterfaceKitPhidget, PhidgetException}
+
 /**
   * Created by bri_e on 20-04-17.
   * The field can be watered xor heated
   */
-class Field {
+class Field(interfaceKitPhidget: InterfaceKitPhidget, indexRed: Int, indexBlue: Int, indexGreen: Int) {
+  var is_Watering:Boolean = false
+  var is_Heating: Boolean = false
 
+  def isHeating:Boolean = isHeating
+  def isWatering:Boolean = isWatering
+
+  def setWatering(state:Boolean):Unit = {
+    is_Watering = state
+    if (is_Watering) is_Heating = false
+    //setLight()
+  }
+
+  def setHeating(state: Boolean): Unit = {
+    is_Heating = state
+    if (is_Heating) is_Watering = false
+    //setLight()
+  }
+
+  private def setLight():Unit = {
+    if (is_Watering)
+      if (is_Heating)
+        println("Erreur : état incorrect !!")
+      else {
+        interfaceKitPhidget.setOutputState(indexGreen, false)
+        interfaceKitPhidget.setOutputState(indexRed, false)
+        interfaceKitPhidget.setOutputState(indexBlue, true)
+      }
+
+    if (is_Heating)
+      if (is_Watering)
+        println("Erreur : état incorrect !!")
+      else {
+        interfaceKitPhidget.setOutputState(indexBlue, false)
+        interfaceKitPhidget.setOutputState(indexGreen, false)
+        interfaceKitPhidget.setOutputState(indexRed, true)
+      }
+  }
 }
