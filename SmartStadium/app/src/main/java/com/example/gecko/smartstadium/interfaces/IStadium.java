6634342324579e4af1ec046c4bbb624a1db80b5e
Jordan Andreux <jordan.andreux@student.unamur.be>
2017-04-21 11:21:35 +0200

    [Android] New WebServices

--- SmartStadium/app/src/main/java/com/example/gecko/smartstadium/interfaces/IStadium.java
+++ SmartStadium/app/src/main/java/com/example/gecko/smartstadium/interfaces/IStadium.java
@@ -2,9 +2,12 @@ package com.example.gecko.smartstadium.interfaces;
 
 import com.example.gecko.smartstadium.classes.Athletic;
 import com.example.gecko.smartstadium.classes.Credentials;
+import com.example.gecko.smartstadium.classes.Lap;
+import com.example.gecko.smartstadium.classes.Match;
 import com.example.gecko.smartstadium.classes.OccupancyRate;
 import com.example.gecko.smartstadium.classes.Refreshment;
 import com.example.gecko.smartstadium.classes.Seat;
+import com.example.gecko.smartstadium.classes.Spectator;
 import com.example.gecko.smartstadium.classes.Tribune;
 
 import java.util.ArrayList;
@@ -20,6 +23,12 @@ public interface IStadium {
     //ATHLETICS
     @GET("athletics/{id}")
     Call<Athletic> getAthletic(@Field("id") String id);
+
+    @GET("athletics/{id}/races/last/laps/last")
+    Call<Lap> getAthleticLastLapFromLastRace(@Field("id") String id);
+
+    @GET("athletics/{id}/matchs/ended")
+    Call<ArrayList<Match>> getAthleticMatchsNotEnded(@Field("id") String id);
 
     @POST("athletics/login")
     Call<Athletic> postLogin(@Body Credentials credentials);
@@ -43,4 +52,8 @@ public interface IStadium {
 
     @GET("tribunes/{id}/available")
     Call<ArrayList<Seat>> getTribuneSeatsAvailable(@Field("id") int id);
+
+    //SPECTATORS
+    @GET("spectators/id")
+    Call<Spectator> getSpectator(@Field("id") int id);
 }
