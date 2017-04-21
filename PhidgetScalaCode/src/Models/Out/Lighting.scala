d11d5b32c4ab70a4a93a7e756905afa30fc862dc
Brieuc <brieuc.olivier@student.unamur.be>
2017-04-21 10:54:54 +0200

    ouptut done

--- PhidgetScalaCode/src/Models/Out/Lighting.scala
+++ PhidgetScalaCode/src/Models/Out/Lighting.scala
@@ -1,9 +1,43 @@
 package Models.Out
 
+import com.phidgets.InterfaceKitPhidget
+
 /**
   * Created by bri_e on 20-04-17.
   * The lighting is moderated in relation with the ambient light power
   */
-class Lighting {
+class Lighting(interfaceKitPhidget: InterfaceKitPhidget) {
+  val connectedLEDS:Int = 5
+  var power: Int = 0
 
+  def updatePower(lightIntensity:Int):Unit = {
+    power = 100/lightIntensity
+    refreshLighting
+  }
+
+  def refreshLighting:Unit = {
+    var rest = power
+    val step = 100 / connectedLEDS
+    var ledNumber = 0
+    while (step <= rest) {
+      ledNumber += 1
+      rest -= step
+    }
+
+    var x = 0
+    while (x < ledNumber) {
+      if (!interfaceKitPhidget.getOutputState(x)) {
+        interfaceKitPhidget.setOutputState(x, true)
+        x += 1
+      }
+    }
+
+    var i = ledNumber
+    while (i < connectedLEDS) {
+      if (interfaceKitPhidget.getOutputState(i)) {
+        interfaceKitPhidget.setOutputState(i, false)
+        i += 1
+      }
+    }
+  }
 }
--- PhidgetScalaCode/src/Models/Out/Roof.scala
+++ PhidgetScalaCode/src/Models/Out/Roof.scala
@@ -1,10 +1,47 @@
 package Models.Out
 
+import com.phidgets.AdvancedServoPhidget
+
 /**
   * Created by bri_e on 20-04-17.
   * The roof can be opened xor closed.
   * Both ServoMotor works in coalition in order to close the roof. 
   */
 class Roof {
+  //Todo : Add 2nd ServoMotor
+
+  val SERVO_SERIAL:Int = 305832
+  val SERVO_INDEX:Int = 0
+  val VAL_ROOF_CLOSED:Double = 15.00
+  val VAL_ROOF_OPEN:Double = 220.00
+
+  var open:Boolean = false
+  val servo:AdvancedServoPhidget = new AdvancedServoPhidget
+
+  //Add listeners for error
+
+  servo.open(SERVO_SERIAL)
+  servo.waitForAttachment()
+  servo.setEngaged(SERVO_INDEX, true)
+  servo.setPosition(SERVO_INDEX, VAL_ROOF_CLOSED)
+
+  def closeRoof:Unit = {
+    if (open) {
+      servo.setPosition(SERVO_INDEX, VAL_ROOF_CLOSED)
+      println("      ======> roof closed")
+      open = false
+    } else
+      println("      ======> roof already closed")
+  }
+
+  def openRoof:Unit = {
+    if (!open) {
+      servo.setPosition(SERVO_INDEX, VAL_ROOF_OPEN)
+      println("      ======> roof opened")
+      open = true
+    } else
+      println("      ======> roof already opened")
+  }
+
 
 }
