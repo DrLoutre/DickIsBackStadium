5adfc87a1e5f3bbbde215a53a1d9ae06254fb3f3
DESKTOP-6GD5MQQ\Gecko <quentin.jacquemotte@student.unamur.be>
2017-04-22 18:11:35 +0200

    [Android] list des matchs ( pas encore link to DB)

--- SmartStadium/app/src/main/java/com/example/gecko/smartstadium/activities/StatActivity.java
+++ SmartStadium/app/src/main/java/com/example/gecko/smartstadium/activities/StatActivity.java
@@ -4,10 +4,13 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.gecko.smartstadium.R;
+import com.example.gecko.smartstadium.adapter.MatchAdapter;
 import com.example.gecko.smartstadium.bus.BusProvider;
 import com.example.gecko.smartstadium.events.AthleticEvent;
 import com.example.gecko.smartstadium.events.GetLastRaceAthleticEvent;
@@ -27,6 +30,9 @@ public class StatActivity extends AppCompatActivity {
     private TextView tempsText;
     private TextView equipeText;
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter matchAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,28 @@ public class StatActivity extends AppCompatActivity {
         tempsText = (TextView) findViewById(R.id.textTpsStat);
         equipeText = (TextView) findViewById(R.id.textTeamStat);
 
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.matchListStat);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //todo tmp il faudra mettre ca où on aura l'info
+        String[] list = new String[5];
+        list[1] = "coucou1";
+        list[2] = "coucou2";
+        list[3] = "coucou3";
+        list[4] = "coucou4";
+        list[0] = "coucou0";
+        // specify an adapter (see also next example)
+        matchAdapter = new MatchAdapter(list);
+        mRecyclerView.setAdapter(matchAdapter);
+
+
+
+
+
         dialog = new ProgressDialog(this);
         dialog.setIndeterminate(true);
         dialog.setCanceledOnTouchOutside(false);
@@ -47,6 +75,7 @@ public class StatActivity extends AppCompatActivity {
         tempsText.setText("");
 
         getAthletic();
+
     }
 
     @Override
@@ -106,4 +135,6 @@ public class StatActivity extends AppCompatActivity {
         }
         dialog.dismiss();
     }
+
+
 }
---
+++ SmartStadium/app/src/main/java/com/example/gecko/smartstadium/adapter/MatchAdapter.java
@@ -0,0 +1,67 @@
+package com.example.gecko.smartstadium.adapter;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.gecko.smartstadium.R;
+
+/**
+ * Created by Quentin Jacquemotte on 22-04-17.
+ */
+
+public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.ViewHolder> {
+
+    private String[] mDataset;
+
+
+    public MatchAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MatchAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.match, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.domicile.setText(mDataset[position]);
+        holder.visiteur.setText(mDataset[position]);
+        holder.date.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+
+    // Provide a reference to the views for each data item
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        CardView card;
+        TextView date;
+        TextView domicile;
+        TextView visiteur;
+
+        public ViewHolder(View v) {
+            super(v);
+            card = (CardView) itemView.findViewById(R.id.matchCard);
+            date = (TextView) itemView.findViewById(R.id.dateMatchCard);
+            domicile = (TextView) itemView.findViewById(R.id.domicieMatchCard);
+            visiteur = (TextView) itemView.findViewById(R.id.visiteurMatchCard);
+        }
+    }
+}
