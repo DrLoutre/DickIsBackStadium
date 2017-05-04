924db25265b6acee7a46a3faadbd279fbfc7ce9d
Maxime Hendrix <maxime.hendrix41@hotmail.com>
2017-05-04 12:56:04 +0200

    [Projet JMJ] correction de l'incompréhension relative au projet

--- Partie_JMJ/Api_langageD/src/Api_langageD.scala
+++ Partie_JMJ/Api_langageD/src/Api_langageD.scala
@@ -1,65 +1,53 @@
-import scala.util.parsing.combinator.RegexParsers
-
-class BachTParsersAPI extends RegexParsers {
-
-  def token 	: Parser[String] = ("[a-z][0-9a-zA-Z_]*").r ^^ {_.toString}
-  def duration 	: Parser[Int] = ("[0-9]*").r ^^ {_.toInt}
-
-  val opChoice  : Parser[String] = "|||"
-  val opPara    : Parser[String] = "|"
-  val opSeq     : Parser[String] = "//"
-
-  def primitive : Parser[String]   = "tell("~token~")" ^^ {
-    case _ ~ vtoken ~ _  => "tell(" + vtoken + ")" }  |
-    "ask("~token~")" ^^ {
-      case _ ~ vtoken ~ _  => "ask(" + vtoken + ")" }   |
-    "get("~token~")" ^^ {
-      case _ ~ vtoken ~ _  => "get(" + vtoken + ")" }   |
-    "nask("~token~")" ^^ {
-      case _ ~ vtoken ~ _  => "nask(" + vtoken + ")" } |
-    "delay("~duration~")" ^^ {
-      case _ ~ vtoken ~ _  => "delay(" + vtoken + ")" }
-  def agent = compositionChoice
-
-  def compositionChoice : Parser[String] = compositionPara~rep(opChoice~compositionChoice) ^^ {
-    case ag ~ List() => ag
-    case agi ~ List(op~agii)  => agi + "+" + agii }
-
-  def compositionPara : Parser[String] = compositionSeq~rep(opPara~compositionPara) ^^ {
-    case ag ~ List() => ag
-    case agi ~ List(op~agii)  => agi + "||" + agii }
-
-  def compositionSeq : Parser[String] = simpleAgent~rep(opSeq~compositionSeq) ^^ {
-    case ag ~ List() => ag
-    case agi ~ List(op~agii)  => agi + ";" + agii }
-
-  def simpleAgent : Parser[String] = primitive | parenthesizedAgent
-
-  def parenthesizedAgent : Parser[String] = "("~>agent<~")"
-
-}
-
-object BachTSimulParserAPI extends BachTParsersAPI {
-
-  def parse_primitive(prim: String) = parseAll(primitive,prim) match {
-    case Success(result, _) => result
-    case failure : NoSuccess => scala.sys.error(failure.msg)
-  }
-
-  def parse_agent(ag: String) = parseAll(agent,ag) match {
-    case Success(result, _) => result
-    case failure : NoSuccess => scala.sys.error(failure.msg)
-  }
-
-}
+class Extension
+case class tell(token: String) extends Extension
+case class ask(token: String) extends Extension
+case class nask(token: String) extends Extension
+case class get(token: String) extends Extension
+case class delay(time: Int) extends Extension
+case class &&(agenti: Extension, agentii: Extension) extends Extension
+case class |(agenti: Extension, agentii: Extension) extends Extension
+case class |||(agenti: Extension, agentii: Extension) extends Extension
 
 object api {
 
-  def apply(agent: String) {
-    val agent_parsed = BachTSimulParserAPI.parse_agent(agent)
-    //ag run(agent_parsed) lien vers BachtT
+  def apply(agent: Extension) {
+    val agent_parsed = toStringAgent(agent)
   }
 
-  def eval(agent:String) { apply(agent) }
-  def run(agent:String) { apply(agent) }
+  def eval(agent: Extension) {
+    apply(agent)
+  }
+
+  def run(agent: Extension) {
+    apply(agent)
+  }
+
+  def toStringAgent(agent: Extension): String = {
+      agent match {
+        case tell(token) => {
+          "tell(" + token + ")"
+        }
+        case ask(token) => {
+          "ask(" + token + ")"
+        }
+        case nask(token) => {
+          "nask(" + token + ")"
+        }
+        case get(token) => {
+          "get(" + token + ")"
+        }
+        case delay(time) => {
+          "delay(" + time + ")"
+        }
+        case |||(ag_1, ag_2) => {
+          "(" + toStringAgent(ag_1) + ")+(" + toStringAgent(ag_2) + ")"
+        }
+        case &&(ag_1, ag_2) => {
+          toStringAgent(ag_1) + ";" + toStringAgent(ag_2)
+        }
+        case |(ag_1, ag_2) => {
+          "(" + toStringAgent(ag_1) + ")||(" + toStringAgent(ag_2) + ")"
+        }
+      }
+  }
 }
