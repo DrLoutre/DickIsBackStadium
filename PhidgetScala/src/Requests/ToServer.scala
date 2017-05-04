83a33230ed50e7c4498cbfd9005d7543bd5b75f1
Jordan Andreux <jordan.andreux@student.unamur.be>
2017-05-04 19:35:24 +0200

    New Beans and Scala code

--- PhidgetScala/src/Requests/ToServer.scala
+++ PhidgetScala/src/Requests/ToServer.scala
@@ -4,16 +4,18 @@ import scalaj.http.Http
 
 object ToServer {
 
-  private val BASE_URL = ""
+  //TODO : Change URL when deploy
+  private val BASE_URL = "http://localhost:8080/StadiumWebSite/api/v1/communication/"
 
-  def sendLap(rfid:String, nbrTours:Int, tempsTour:String) : Unit = {
-    val json:String = new Gson().toJson()
+  def sendLap(rfid:String, nbrTours:Int, tempsTour:String, tempsMilli:Int) : Unit = {
+    val lap:Lap = new Lap(rfid, nbrTours, tempsTour, tempsMilli)
+    val json:String = new Gson().toJson(lap)
     val code:Boolean = Http(BASE_URL.concat("laps")).timeout(10000, 10000).postData(json).asString.is2XX
   }
 
   def sendGoal(isDroit:Boolean, nbrGoals:Int) : Unit = {
     val goal:Goal = new Goal(isDroit, nbrGoals)
     val json:String = new Gson().toJson(goal)
-    Http(BASE_URL.concat("goals")).timeout(10000, 10000).postData().asString.is2xx
+    val code:Boolean = Http(BASE_URL.concat("goals")).timeout(10000, 10000).postData(json).asString.is2xx
   }
 }
