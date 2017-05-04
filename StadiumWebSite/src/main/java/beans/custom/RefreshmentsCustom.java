22ec9cc586a62d41a0a5ca9b332e31193cc8ad74
Jordan Andreux <jordan.andreux@student.unamur.be>
2017-05-04 17:48:30 +0200

    [Oups] J’ai oublié de push ça

--- StadiumWebSite/src/main/java/beans/custom/RefreshmentsCustom.java
+++ StadiumWebSite/src/main/java/beans/custom/RefreshmentsCustom.java
@@ -1,4 +1,26 @@
 package beans.custom;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class RefreshmentsCustom {
+
+    private int attendance1;
+    private int attendance2;
+
+    public int getAttendance1() {
+        return attendance1;
+    }
+
+    public void setAttendance1(int attendance1) {
+        this.attendance1 = attendance1;
+    }
+
+    public int getAttendance2() {
+        return attendance2;
+    }
+
+    public void setAttendance2(int attendance2) {
+        this.attendance2 = attendance2;
+    }
 }
