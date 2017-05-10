6aafb34e080ce3913f2e3e05a2301f36274a62ed
Brieuc <brieuc.olivier@student.unamur.be>
2017-05-10 17:01:10 +0200

    patch

--- PhidgetScala/src/BlackBox/BlackBox.scala
+++ PhidgetScala/src/BlackBox/BlackBox.scala
@@ -90,8 +90,12 @@ class BlackBox(interfaceKitPhidget: InterfaceKitPhidget){
     log += "Goals : " + goal.goal + "\n"
     log += "Field : " + (if(field.is_Heating) "heating " else " not heating" ) + " and "  + (if (field.is_Watering) "watering" else "not watering") + "\n"
     log += "Roof  : " + (if(roof.open) "openned" else "closed") + "\n"
-    log = currentMode match {
-      case DetachedMode(kit, roof, rfid) => log + "Kit is detachd? " + kit + "\nRoof is detached? " + roof + "\nRFID is detached? " + rfid + "\n"
+    currentMode match {
+      case DetachedMode(kit, rf, rfid) =>
+        log += "Kit is detachd? " + kit + "\nRoof is detached? " + rf + "\nRFID is detached? " + rfid + "\n"
+        log += "Is Kit REALLY DETACHED =>   Attached? "+ interfaceKitPhidget.isAttached
+        if (interfaceKitPhidget.isAttached && rf && rfid)
+          currentMode = mode.getCurrentMode
       case _ => log + currentMode.toString + "\n"
     }
 
