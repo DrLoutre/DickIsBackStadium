92a3327cd2ce804c3b6c76a34ca1ca3214d6d6c9
Brieuc <brieuc.olivier@student.unamur.be>
2017-05-11 17:42:05 +0200

    lmksfdjmlskdqfj

--- PhidgetScala/src/Main.scala
+++ PhidgetScala/src/Main.scala
@@ -1,5 +1,9 @@
+
+import java.io.FileInputStream
+
 import BlackBox.BlackBox
 import com.phidgets.InterfaceKitPhidget
+import sun.audio.{AudioPlayer, AudioStream}
 
 /**
   * Created by bri_e on 20-04-17.
@@ -7,6 +11,21 @@ import com.phidgets.InterfaceKitPhidget
   */
 object Main {
   def main(args: Array[String]): Unit = {
+
+    try {
+      // open the sound file as a Java input stream
+      val gongFile = "D:/begin.wav"
+      val in = new FileInputStream(gongFile)
+
+      // create an audiostream from the inputstream
+      val audioStream = new AudioStream(in)
+
+      // play the audio clip with the audioplayer class
+      AudioPlayer.player.start(audioStream)
+    } catch {
+      case e:Exception => println("Exception while opening theme" + e)
+    }
+
     print("Initialising the InterfaceKit Phidget . . . ")
     var interfaceKitPhidget:InterfaceKitPhidget = new InterfaceKitPhidget
     interfaceKitPhidget.open(329107)
