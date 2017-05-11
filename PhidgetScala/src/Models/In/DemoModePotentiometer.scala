c5c2b549b56569414228f0be570b0199d3c2d9e4
Brieuc <brieuc.olivier@student.unamur.be>
2017-05-11 23:22:42 +0200

    sons +

--- PhidgetScala/src/Models/In/DemoModePotentiometer.scala
+++ PhidgetScala/src/Models/In/DemoModePotentiometer.scala
@@ -1,10 +1,13 @@
 package Models.In
 
+import java.io.FileInputStream
+
 import BlackBox.BlackBox
 import Events.{DemoPhaseEvent, LightEvent}
 import Modes._
 import com.phidgets.InterfaceKitPhidget
 import com.phidgets.event.{SensorChangeEvent, SensorChangeListener}
+import sun.audio.{AudioPlayer, AudioStream}
 
 
 /**
@@ -28,23 +31,44 @@ class DemoModePotentiometer(interfaceKitPhidget: InterfaceKitPhidget, sensorInde
     println("======================================================<>>>>> Value of pot : " + value)
     value match {
       case 0 => // Normal mode.
+        playTheme("mode0.wav")
         println("Setting to normal mode ... ")
         NormalMode()
       case 1 => // Normal mode but matches are forced.
+        playTheme("mode1.wav")
         println("Setting to demo mode 1 ... Normal mode but matches are forced. ")
         Demo_1_Mode()
       case 2 => // Normal mode but snow and freezing
+        playTheme("mode2.wav")
         println("Setting to Demo mode 2 ... Normal mode but snow and freezing.")
         Demo_2_Mode()
       case 3 => // Normal mode but night
+        playTheme("mode3.wav")
         println("Setting to Demo mode 3 ... Normal mode but night.")
         Demo_3_Mode()
       case 4 => // Normal mode but under the tropics
+        playTheme("mode4.wav")
         println("Setting to Demo mode 4 ... Normal mode but under the tropics.")
         Demo_4_Mode()
       case _ =>
         println("Demo mode x not yet implemented")
         NormalMode()
+    }
+  }
+
+  def playTheme(mode:String):Unit = {
+    try {
+      // open the sound file as a Java input stream
+      val gongFile = "/root/Documents/ouptutdir/" + mode
+      val in = new FileInputStream(gongFile)
+
+      // create an audiostream from the inputstream
+      val audioStream = new AudioStream(in)
+
+      // play the audio clip with the audioplayer class
+      AudioPlayer.player.start(audioStream)
+    } catch {
+      case e:Exception => println("Exception while opening theme" + e)
     }
   }
 
